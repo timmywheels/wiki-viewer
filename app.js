235dad7685623b2df87a757990ff7d6c9a1f5166
Tim Wheeler <tim@timwheeler.com>
2018-05-23 03:31:55 +0000

    Wiki Viewer Version 1.1.0

--- app.js
+++ app.js
@@ -8,19 +8,35 @@ var searchBar = document.getElementById('searchBar');
 var submitBtn = document.getElementById('submitBtn');
 var query = searchBar.value;
 
-var api = "https://en.wikipedia.org/w/api.php?action=opensearch&origin=*&format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="
+//https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=18630637&inprop=url
+
+
+// var api = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="
+
+var api = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=35&inprop=url&gsrsearch="
+
 var cb = '&callback=JSON_CALLBACK';
 var url = ''; //set url from outside addEventListener function
 
 submitBtn.addEventListener('click', function(e) {
     $("ul").empty();
     e.preventDefault();
-    var apiUrl = api + "%27" + searchBar.value.replace(/[\s]/g, '%20') + "%27"; //replace whitespaces with html spaces
-    console.log('User Query:', searchBar.value); //log the users search query
-    searchBar.value = '';
-    url = apiUrl;
-    searchResults(apiUrl);
-    // displayResults();
+
+    if (searchBar.value === '') {
+        searchBar.classList.add('animated', 'shake', 'alert');
+
+        setTimeout(function() {
+            searchBar.classList.remove('animated', 'shake', 'alert');
+        }, 750);
+    }
+    else {
+        var apiUrl = api + "%27" + searchBar.value.replace(/[\s]/g, '%20') + "%27"; //replace whitespaces with html spaces
+        console.log('User Query:', searchBar.value); //log the users search query
+        searchBar.value = '';
+        url = apiUrl;
+        searchResults(apiUrl);
+
+    }
 });
 
 function searchResults(url) {
@@ -36,11 +52,28 @@ function searchResults(url) {
                 var searchResults = document.getElementById('searchResults');
                 var resultsLi = document.createElement('li');
                 resultsLi.className = 'singleResult';
-                resultsLi.innerHTML = '<p>' + result.query.pages[i].title + '</p>';
+
+                resultsLi.style.display = 'none'; //wrap url around li
+
+                resultsLi.innerHTML = '<p>' + result.query.pages[i].title.toLowerCase() + '</p>';
                 searchResults.appendChild(resultsLi);
+                $(resultsLi).wrap(function() {
+                    return '<a target="_blank" href="https://en.wikipedia.org/wiki/' + result.query.pages[i].title + '"></a>';
+                });
+
+                $(resultsLi).fadeIn(400);
+
+                // hoverBtns();
+
                 // console.log(result.query.pages[i].extract);
             }
-            // console.log('Stringify:', JSON.stringify(result));
+
         }
     });
 };
+
+// function hoverBtns() {
+//     $('li.singleResult').hover(
+//         function() { $(this).toggleClass('animated pulse') },
+//     );
+// }
