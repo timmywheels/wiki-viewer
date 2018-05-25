f18ec50145bc7ffab610dcc87aa6b40a1575b5a6
Tim Wheeler <tim@timwheeler.com>
2018-05-25 01:21:25 +0000

    Wiki Viewer Version 1.1.2

--- app.js
+++ app.js
@@ -2,77 +2,70 @@
 // https://en.wikipedia.org/wiki/Special:Random
 // https://www.mediawiki.org/wiki/API:Main_page
 
-/* global $ */
-
 var searchBar = document.getElementById('searchBar');
 var submitBtn = document.getElementById('submitBtn');
 var query = searchBar.value;
 
-//https://en.wikipedia.org/w/api.php?action=query&prop=info&pageids=18630637&inprop=url
+var api = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=35&gsrsearch="
 
+// var cb = '&callback=JSON_CALLBACK';
+var url = ''; // Set url from outside addEventListener function
 
-// var api = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&inprop=url&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch="
+submitBtn.addEventListener('click', function(e) { // Listen for click on submit button
 
-var api = "https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&generator=search&gsrnamespace=0&gsrlimit=35&inprop=url&gsrsearch="
+    $("#searchResults").empty(); // Clear search results
 
-var cb = '&callback=JSON_CALLBACK';
-var url = ''; //set url from outside addEventListener function
+    e.preventDefault(); // Prevent default behavior of submit button
 
-submitBtn.addEventListener('click', function(e) {
-    $("ul").empty();
-    e.preventDefault();
+    if (searchBar.value === '') { // If search bar is empty
+        searchBar.classList.add('animated', 'shake', 'alert'); // Add the alert class
 
-    if (searchBar.value === '') {
-        searchBar.classList.add('animated', 'shake', 'alert');
-
-        setTimeout(function() {
+        setTimeout(function() { // Remove alert after animation complete
             searchBar.classList.remove('animated', 'shake', 'alert');
         }, 750);
     }
+
     else {
-        var apiUrl = api + "%27" + searchBar.value.replace(/[\s]/g, '%20') + "%27"; //replace whitespaces with html spaces
-        console.log('User Query:', searchBar.value); //log the users search query
-        searchBar.value = '';
-        url = apiUrl;
-        searchResults(apiUrl);
+
+        var apiUrl = api + "%27" + searchBar.value.replace(/[\s]/g, '_') + "%27"; // Replace whitespaces with underscores
+
+        // console.log(apiUrl);
+        // console.log('User Query:', searchBar.value); // Log the users search query
+        searchBar.value = ''; // Clear search bar
+        url = apiUrl; // Set url to apiUrl
+        searchResults(apiUrl); // Call searchResults, passing in the apiUrl
 
     }
+
 });
 
 function searchResults(url) {
+
     $.ajax({
         url: url,
         success: function(result) {
 
-            console.log('Result:', result); //returns full result object
-            console.log('Pages:', result.query.pages); //returns result pages within result object
+            //console.log('Result:', result); // Returns full result object
+            //console.log('Pages:', result.query.pages); // Returns result pages within result object
 
-            for (var i in result.query.pages) { //loop through all pages
-                console.log(result.query.pages[i].title);
+            for (var i in result.query.pages) { // Loop through all pages within result object
+
+                // console.log(result.query.pages[i].title);
                 var searchResults = document.getElementById('searchResults');
-                var resultsLi = document.createElement('li');
-                resultsLi.className = 'singleResult';
+                var resultsLi = document.createElement('li'); // Create li element for all page titles
 
-                resultsLi.style.display = 'none'; //wrap url around li
+                resultsLi.className = 'singleResult'; // Add class to all li elements
+                resultsLi.style.display = 'none'; // Hide li by default
+                resultsLi.innerHTML = '<p>' + result.query.pages[i].title.toLowerCase() + '</p>'; // Add title text to lis
+                searchResults.appendChild(resultsLi); // Append lis to searchResults div
 
-                resultsLi.innerHTML = '<p>' + result.query.pages[i].title.toLowerCase() + '</p>';
-                searchResults.appendChild(resultsLi);
-                $(resultsLi).wrap(function() {
+                $(resultsLi).wrap(function() { // Wrap li with corresponding wiki url
                     return '<a target="_blank" href="https://en.wikipedia.org/wiki/' + result.query.pages[i].title + '"></a>';
                 });
-                $(resultsLi).fadeIn(1000);
 
-                // hoverBtns();
-
-                // console.log(result.query.pages[i].extract);
+                $(resultsLi).fadeIn(1000); // Fade in hidden lis
             }
 
         }
     });
 };
-
-// function hoverBtns() {
-//     $('li.singleResult').hover(
-//         function() { $(this).toggleClass('animated pulse') },
-//     );
-// }
